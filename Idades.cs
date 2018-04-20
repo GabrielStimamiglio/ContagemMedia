f8fb2b09e5f3745cc421192f551145a85313bea6
GabrielStima <34146939+GabrielStima@users.noreply.github.com>
2018-04-20 08:28:34 -0300

    Teste 1

---
+++ Idades.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContagemMedia
+{
+    class Idades
+    {
+        double media;
+        int soma;
+        List<int> idade = new List<int>();
+
+        public Idades()
+        {
+            
+        }
+
+        public double getMedia()
+        {
+            return media;
+        }
+        public int getSoma()
+        {
+            return soma;
+        }
+        public void calcularMedia()
+        {
+            media = media / indice;
+        }
+
+        public void somar(int idade)
+        {
+            soma = soma + idade;
+        }
+
+        public void cadastrarIdade(int novaIdade)
+        {
+            idade.Add(novaIdade);  
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContagemMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op;
+            
+
+            Idades i = new Idades();
+
+            
+            Console.WriteLine("1 - Cadastrar Idade ");
+            Console.WriteLine("2 - Calcular media ");
+            op = int.Parse(Console.ReadLine());
+            do
+            {
+
+                switch (op)
+                {
+                    case 1:
+
+
+                        Console.WriteLine("--------------menu-------------");
+                        Console.WriteLine("1 - Cadastrar Idade ");
+                        Console.WriteLine("2 - Calcular media ");
+                        Console.WriteLine("0 - Sair ");
+                        op = int.Parse(Console.ReadLine());
+                        break;
+                }
+            } while (op != 0);
+        }
+    }
+}
